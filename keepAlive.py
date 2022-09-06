7ec9e4da1344e2457d5de8c68c359dcb791f7193
Gabriel Reus <Gabriel.Reus@gmail.com>
2022-09-06 08:50:15 +0200

    Commit inicial

---
+++ keepAlive.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env  python3
+
+#pip install pyautogui
+
+#import library pyautogui
+import pyautogui
+
+#import library time
+import time
+
+
+while True:
+    #move curor 10 pixels
+    pyautogui.moveRel(0,10)
+    #pause your code from running 2 seconds.
+    time.sleep(2)
